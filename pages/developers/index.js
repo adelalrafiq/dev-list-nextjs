c4e90bcad5d356ba405149b8229a1b24d1a2f9ad
adelalrafiq <adelalrafiq@gmail.com>
2021-05-30 23:01:25 +0200

    Fetching Data (getStaticProps)

--- pages/developers/index.js
+++ pages/developers/index.js
@@ -1,7 +1,23 @@
-const Developers = () => {
+import styles from '../../styles/Developers.module.css';
+
+export const getStaticProps = async ()=>{
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+
+  return {
+    props:{ developers: data }
+  }
+}
+
+const Developers = ({developers}) => {
   return ( 
     <div>
       <h1>All developers</h1>
+      {developers.map(developer => (
+        <div key= {developer.id}>
+          <a className={styles.single}><h3>{developer.name}</h3></a>
+        </div>
+      ))}
     </div>
    );
 }
