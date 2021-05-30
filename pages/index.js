4937ee15019c4ef4d7e2af623de0982e2c41e0b8
adelalrafiq <adelalrafiq@gmail.com>
2021-05-30 11:54:46 +0200

    creating a layout component

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,13 @@ import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 export default function Home() {
   return (
-    <div>
-      <Navbar />
+    <div>      
       <h1>Home Page</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus laudantium, ducimus est, vitae similique ipsam odit officia nesciunt modi dolorem distinctio facilis aut minima natus veniam veritatis maiores enim voluptatem?</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus laudantium, ducimus est, vitae similique ipsam odit officia nesciunt modi dolorem distinctio facilis aut minima natus veniam veritatis maiores enim voluptatem?</p>
       <Link href="/developers">
       <a>See Developers List</a>        
-      </Link>
-      <Footer/>
+      </Link>      
     </div>
   )
 }
