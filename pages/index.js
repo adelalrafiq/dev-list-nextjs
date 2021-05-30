be30697f62f787a30fd8328ee9c84cc5ffaeb285
adelalrafiq <adelalrafiq@gmail.com>
2021-05-30 12:24:49 +0200

    Adding Styles

--- pages/index.js
+++ pages/index.js
@@ -7,11 +7,11 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div>      
-      <h1>Home Page</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus laudantium, ducimus est, vitae similique ipsam odit officia nesciunt modi dolorem distinctio facilis aut minima natus veniam veritatis maiores enim voluptatem?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus laudantium, ducimus est, vitae similique ipsam odit officia nesciunt modi dolorem distinctio facilis aut minima natus veniam veritatis maiores enim voluptatem?</p>
+      <h1 className={styles.title}>Home Page</h1>
+      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus laudantium, ducimus est, vitae similique ipsam odit officia nesciunt modi dolorem distinctio facilis aut minima natus veniam veritatis maiores enim voluptatem?</p>
+      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus laudantium, ducimus est, vitae similique ipsam odit officia nesciunt modi dolorem distinctio facilis aut minima natus veniam veritatis maiores enim voluptatem?</p>
       <Link href="/developers">
-      <a>See Developers List</a>        
+      <a className={styles.btn}>See Developers List</a>        
       </Link>      
     </div>
   )
